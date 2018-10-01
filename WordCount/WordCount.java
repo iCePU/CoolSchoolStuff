0c40e6e20219aec0c6d035604cda483263a7b44e
iCePU <polylewtnoic@gmail.com>
2018-10-01 09:38:44 +0000

    starting the list program

---
+++ WordCount/WordCount.java
@@ -0,0 +1,46 @@
+import java.nio.file.*;
+import java.io.IOException;
+import java.lang.String;
+import java.util.*;
+import java.io.BufferedReader;
+
+public WordCount{
+     public static void WordCount(String argv[] )Throws IOException{
+          //TAKE IN FUCKING IOIOIOIOIOIOIOIOIOIOIIOIOI
+     }
+
+     public String fileRead(String fileName){
+          //cancer
+     }
+
+     public LinkList makeLinkList(String fileString){
+
+     }
+     public LinkList uniqueList(LinkList listy){
+
+     }
+     public int sizeOfList(LinkList listy){
+
+     }
+}
+
+class LinkList{
+     public LinkList head;
+     public LinkList tail;
+     public String data = "";
+     public int frequency;
+
+     LinkList(LinkList head, LinkList tail, String data){
+          this.head = head;
+          this.data = data;
+          this.tail = tail;
+     }
+     LinkList next(){
+
+     }
+
+     LinkList previous(){
+
+     }
+
+}
